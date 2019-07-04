fda02220fcd9ad15962e93ec3f9d2ab7f03c213e
cutBoy <18207307296@163.com>
2019-07-04 17:40:35 +0800

    first commit

---
+++ src/main/java/com/service/config/ConfigServiceApplication.java
@@ -0,0 +1,20 @@
+package com.service.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author hejian
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
